bbe7361ec2e1ac6b32dcc7b894d5df32ee3c666e
Jackson Taylor <Jackson Taylor taylor11@iu.edu>
2022-07-05 09:42:52 -0400

    tests

--- APTJunk/multiprocessing_tests.py
+++ APTJunk/multiprocessing_tests.py
@@ -1,10 +1,11 @@
 # from pathos.multiprocessing import ProcessingPool as Pool
-# from multiprocessing import Pool
-from multiprocessing.pool import ThreadPool as Pool
+from multiprocessing import Pool
+# from multiprocessing.pool import ThreadPool as Pool
+import time
 
 
 def fib(n, m=4):
-    print(m)
+    # print(m)
     # if n == 5:
     #     raise Exception("test error")
     if n == 1 or n == 0:
@@ -18,10 +19,14 @@ def f(x):
 
 
 if __name__ == "__main__":
-    print(fib(6, 2))
-    with Pool(5) as p:
-        print(p.starmap(fib, [(35, 5), (35,), (36, 1)]))
+    t0 = time.monotonic()
+    print(fib(38, 2))
+    print(f"part 1 took {round(time.monotonic() - t0, 1)} seconds")
+    t1 = time.monotonic()
+    with Pool(8) as p:
+        print(p.starmap(fib, [(38, i) for i in range(8)]))
     # print(p.starmap(fib, [(35, 5), 35, (35, 1)]))
     # p = Pool(5)
     # print(p.starmap(fib, [(5, 5), (3,), (2, 1)]))
-    print("done")
+    t2 = time.monotonic()
+    print(f"done as {round(t2-t1, 1)} seconds")
